26744fe9b833a11c39657c4bae22851f06e49605
akademik0305 <tuychiyevakademik@gmail.com>
2024-01-26 00:36:47 +0500

    added about course

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,7 +25,7 @@
     <div class="header-menu" :class="{ active: isOpenMenu }" ref="header_menu">
         <div class="header-menu__main flex items-start">
             <button class="header-menu__close" @click="isOpenMenu = false">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24">
                     <path :fill="dark ? '#fff' : '000'"
                         d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275q-.275-.275-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275q.275.275.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7q-.275.275-.7.275t-.7-.275z" />
                 </svg>
@@ -117,7 +117,10 @@
                 <div class="header-menu__head flex items-center justify-between">
                     <button class="flex items-center justify-start gap-2" v-if="current.name"
                         @click="backPreviusMenu(current.col)">
-                        <img class="w-5 h-5" src="~/assets/images/svg/arrow-back.svg" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 20 20">
+                            <path :fill="dark ? '#fff' : '000'" d="m5.83 9l5.58-5.58L10 2l-8 8l8 8l1.41-1.41L5.83 11H18V9z" />
+                        </svg>
+                        <!-- :fill="dark ? '#fff' : '000'" -->
                         Orqaga
                     </button>
                     <h3 class="header-menu__title mr-8" v-if="current.name"> {{ current.name }} </h3>
--- pages/about-course/[id].vue
+++ pages/about-course/[id].vue
@@ -15,24 +15,47 @@
     </div>
     <div class="about-course">
         <div class="container">
-            <div class="about-course-left">
-                <h2 class="about-course-title">{{ details?.data?.title }}</h2>
-                <img :src="details?.data?.image" alt="">
-                <p v-html="details?.data?.body"></p>
+            <h2 class="about-course__title">{{ details?.data?.title }}</h2>
+            <div class="about-course__row">
+                <div class="about-course__left">
+                    <div class="about-course__img">
+                        <img :src="details?.data?.image" alt="">
+                    </div>
+                    <p class="about-course__text" v-html="details?.data?.body"></p>
+                </div>
+                <div class="about-course__right other-categories">
+                    <h3 class="other-categories__title">
+                        Barcha yo'nalishlar:
+                    </h3>
+                    <div class="other-categories__row">
+                        <NuxtLink :to="`/course/${category.id}`" class="other-categories__item" :class="{'active': details?.data?.category === category.id}" v-for="category in categories" :key="category.id">
+                            {{ category.name }}
+                        </NuxtLink>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</template>
+</template>¸
 
 <script setup>
 import Service from "~/services/Service";
 const details = ref({})
+const categories = ref([])
 const { id } = useRoute().params
 async function getDetials() {
     const res = await Service.getAboutCourse(id)
     details.value = res.data
 }
+
+async function getCourseCategories() {
+    const res = await Service.getCourseCategorys()
+    categories.value = res.data.results
+    console.log(categories.value);
+}
+
 getDetials()
+getCourseCategories()
 </script>
 
 <style lang="scss" scoped></style>
