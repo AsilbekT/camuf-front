1974737e657330e70d49626cd407091861373df9
Ismoiljon <ibahtiyorov007@gmail.com>
2024-02-28 13:08:40 +0500

    update project

--- pages/course/[id].vue
+++ pages/course/[id].vue
@@ -15,17 +15,19 @@
     </div>
     <div class="courses-main">
         <div class="container">
-            <h2 class="courses-main__title">{{ $t('BachelorMajors') }}</h2>
+            <h2 class="courses-main__title">{{ $t('Directions') }}</h2>
             <div class="courses-main__items-wrapper">
                 <div class="courses-main__item" v-for="item in courses?.results" :key="item">
                     <div class="courses-main__item-img">
                         <img :src="item?.image" alt="">
                     </div>
                     <div class="courses-main__item-text-wrapper">
-                        <NuxtLink :to="`/about-course/${item?.id}`" class="courses-main__item-title">{{ item?.title }}
+                        <NuxtLink :to="`/about-course/${item?.id}`" class="courses-main__item-title">
+                            <span v-html="item?.title"></span>
                         </NuxtLink>
                         <h4 class="courses-main__item-code">{{ $t('DestinationCode') }}: {{ item?.course_id }}</h4>
-                        <NuxtLink :to="`/about-course/${item?.id}`" class="courses-main__item-btn">{{ $t('DetailedInformation') }}
+                        <NuxtLink :to="`/about-course/${item?.id}`" class="courses-main__item-btn">{{
+                            $t('DetailedInformation') }}
                         </NuxtLink>
                     </div>
                 </div>
@@ -43,7 +45,7 @@ const categorys = ref({})
 const size = ref(6)
 const { id } = useRoute().params
 async function getCourses() {
-    const res = await Service.getCourses(size, id,locale.value)
+    const res = await Service.getCourses(size.value, id, locale.value)
     courses.value = res.data
 }
 async function getCourseCategorys() {
